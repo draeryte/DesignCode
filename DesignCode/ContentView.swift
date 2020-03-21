14c1091fe5e92b08b9b89bdc35e5e8b40cca8f7e
Andrae J Henry <andrae.henry@icloud.com>
2020-03-20 19:25:33 -0500

    Initial Commit

---
+++ DesignCode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignCode
+//
+//  Created by Andrae Henry on 3/20/20.
+//  Copyright © 2020 Andrae Henry. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
